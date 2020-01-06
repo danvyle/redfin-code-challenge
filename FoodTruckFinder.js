e33daa35d7199aba28de1c169cb9b011a04058de
Dan-Vy Le <danvyle85@gmail.com>
2020-01-06 15:20:09 -0800

    fixed duplicate problem

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -2,14 +2,14 @@ const request = require('request');
 const readline = require('readline');
 var API = 'http://data.sfgov.org/resource/bbb8-hzi6.json';
 
+
+const now = new Date();
+var currentDay = now.getDay();
+var currentTime = now.getHours() + ":" + now.getMinutes();
+var returnedResults = [];
+var n = 0
+
 function foodTruckFinder(url) {
-
-  const now = new Date();
-  var currentDay = now.getDay();
-  var currentTime = now.getHours() + ":" + now.getMinutes();
-  var returnedResults = [];
-  var n = 0
-
   request(url, function (error, response, body) {
     if(response.statusCode == 200) {
       JSON.parse(body).forEach(function(key) {
@@ -26,44 +26,48 @@ function foodTruckFinder(url) {
       });
     };
     printResults(n);
-
-
-    function printResults(n) {
-      console.clear();
-      for (i = n; i < n+10; i++) {
-        if (returnedResults[i] === undefined){
-          console.log("This is the end of the list.");
-          process.exit();
-        } else {
-        console.table(returnedResults[i]);
-        }
-      };
-      readLine();
-
-    function readLine() {
-      const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-      });
-      rl.question('Would you like to see the next 10 food truck results? Please answer "y" or "n". ', (answer) => {
-        if(answer === "y"){
-          n+=10;
-          printResults(n);
-        }
-        else if (answer === "n") {
-          console.log("Thanks for visiting the application.");
-          process.exit();
-        }
-
-        else {
-          console.log("Wrong submission. You will exit the program now.")
-          process.exit();
-        }
-      })
-    };
-  }
   });
 };
+
+
+function printResults(n) {
+  console.clear();
+  for (i = n; i < n+10; i++) {
+    if (returnedResults[i] === undefined){
+      console.log("This is the end of the list.");
+      process.exit();
+    } else {
+    console.table(returnedResults[i]);
+    }
+  };
+  readLine();
+}
+
+function readLine() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+  rl.question('Would you like to see the next 10 food truck results? Please answer "y" or "n". ', (answer) => {
+    if(answer === "y"){
+      n+=10;
+      printResults(n);
+    }
+    else if (answer === "n") {
+      console.log("Thanks for visiting the application.");
+      process.exit();
+    }
+
+    else {
+      console.log("Wrong submission. You will exit the program now.")
+      process.exit();
+    }
+  })
+};
+
+
+
 
 
 // else {
