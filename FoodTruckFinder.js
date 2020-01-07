28e81394d0c322d7ae596f7780bf6c047fdf255e
Dan-Vy Le <danvyle85@gmail.com>
2020-01-07 00:59:36 -0800

    final product

--- FoodTruckFinder.js
+++ FoodTruckFinder.js
@@ -1,90 +1,73 @@
+//required
 const request = require('request');
 const readline = require('readline');
-var API = 'http://data.sfgov.org/resource/bbb8-hzi6.json';
 
-
-const now = new Date();
+//current time variables
+var now = new Date();
 var currentDay = now.getDay();
 var currentTime = now.getHours() + ":" + now.getMinutes();
-var returnedResults = [];
+
+//global variables
+const API = 'http://data.sfgov.org/resource/bbb8-hzi6.json';
+var truckCollection = [];
 var n = 0
 
+//API request and sort
 function foodTruckFinder(url) {
   request(url, function (error, response, body) {
     if(response.statusCode == 200) {
       JSON.parse(body).forEach(function(key) {
-        if(currentDay ==  parseInt(key["dayorder"]) && currentTime >= key["start24"] && currentTime <= key["end24"]) {
-          returnedResults.push({"restaurant": key["applicant"], "location": key["location"], "hours": key["dayofweekstr"] + " , " + key["start24"] + " to " +key["end24"]});
+        if(currentDay == parseInt(key["dayorder"]) && currentTime >= key["start24"] && currentTime <= key["end24"]) {
+          truckCollection.push({"Restaurant": key["applicant"], "Location": key["location"], "Hours of Operation": key["dayofweekstr"] + " , " + key["start24"] + " to " +key["end24"]});
         }
-        returnedResults.sort(function(a,b){
-          if(a["restaurant"] > b["restaurant"])
-          return 1;
-          if(a["restaurant"] < b["restaurant"])
-          return -1;
-          return 0;
-        });
+          truckCollection.sort(function(a,b) {
+            if(a["Restaurant"] > b["Restaurant"])
+              return 1;
+            if(a["Restaurant"] < b["Restaurant"])
+              return -1;
+              return 0;
+          });
       });
+    } else {
+      console.log('error:', error);
     };
     printResults(n);
   });
 };
 
-
+//Print first 10 results
 function printResults(n) {
-  console.clear();
   for (i = n; i < n+10; i++) {
-    if (returnedResults[i] === undefined){
+    if (truckCollection[i] === undefined) {
       console.log("This is the end of the list.");
       process.exit();
     } else {
-    console.table(returnedResults[i]);
+      console.table(truckCollection[i]);
     }
   };
-  readLine();
+  next10Trucks();
 }
 
-function readLine() {
+//Prompt user to see next 10
+function next10Trucks() {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
   });
   rl.question('Would you like to see the next 10 food truck results? Please answer "y" or "n". ', (answer) => {
-    if(answer === "y"){
-      n+=10;
+    if(answer === "y") {
+      n += 10;
       printResults(n);
-    }
-    else if (answer === "n") {
-      console.log("Thanks for visiting the application.");
+    } else if (answer === "n") {
+      console.log("Thanks for visiting Food Truck Finder!");
       process.exit();
-    }
-
-    else {
-      console.log("Wrong submission. You will exit the program now.")
+    } else {
+      console.log("Wrong submission. Please start the program over.");
       process.exit();
     }
   })
 };
 
-
-
-
-
-// else {
-//   console.log('error:', error);
-// }
-
-
+//start program
 foodTruckFinder(API);
-// currentDate();
-
-
-
-//get timeout
-// Call the API
-// handle status errors
-// evaluate current time NOW
-// handle body to display 10 trucks with conditionals to evaluate the current time
-// display must be in alphabetical order, use a sort here.
-// prompt the user to go to next page
-// should i allow for user to refresh page?
--- README.md
+++ README.md
@@ -1,41 +1,32 @@
-# Food Truck Finder
+# Food Truck Finder Overview
 
-This is a command line program to help you find a food truck open at the time of running the application. The API used is sourced from the San Francisco government’s website which has a public data source of food trucks here:
+This is a command line program for a user to find a food truck open at the time of running the application. The API used is sourced from the San Francisco government’s website which has a public data source of food trucks here:
 
-https://data.sfgov.org/Economy-and-Community/Mobile-Food-Schedule/jjew-r69b
+[SF Gov public data source for food trucks](https://data.sfgov.org/Economy-and-Community/Mobile-Food-Schedule/jjew-r69b)
+
+The user will see a list of 10 trucks open and prompted to see an additional 10 food trucks if they respond with a "y". Responding with an "n" will exit the program.
 
 ## Installation
-##### OS X
+##### for Mac OS X Users
 
-You will need to make sure you have Node and NPM installed in your environment first, information is available here: https://treehouse.github.io/installation-guides/mac/node-mac.html
+You will need to make sure you have Node and NPM installed in your environment first, information is available here: [Installation info](https://treehouse.github.io/installation-guides/mac/node-mac.html)
 
-If you already have these in your environment, run this in your terminal to install the correct dependencies and start the program
+If you already have these in your environment, type this in your terminal to install the correct dependencies and start the program
 ```
-$ npm install request && node FoodTruckFinder.js
+$ npm install request && npm install readline
 
 ```
-
-
-##### Windows
-coming soon 😝
-
-##### Linux
-tbd
-
 ##### Project Dependencies
 
 request@2.88.0
-readline
+readline@1.3.0
 
-##### How to run
+##### How to Run
+Once you have navigated to the correct directory in the terminal, type the following:
 
 ```
 $ node FoodTruckFinder.js
 ```
 
 
-        "start24": "06:00",
-        "end24": "23:00",
-        "dayorder": "2"
-        "location": "1 MONTGOMERY ST",
-        "applicant": "Treats by the Bay LLC",
+If you plan to use this API heavily, it is advised that you get an App Token and learn more from the Socrata API website: [here](https://dev.socrata.com/docs/app-tokens.html)
